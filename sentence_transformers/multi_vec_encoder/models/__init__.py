7812639fea46ad609fb5aefb1a9ea89ee3fb3815
AymenKallala <aymen.kallala@gmail.com>
2026-01-23 19:24:55 +0100

    move late interaction pooling into dir

--- sentence_transformers/multi_vec_encoder/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from sentence_transformers.multi_vec_encoder.models.LateInteractionPooling import LateInteractionPooling
-
-__all__ = ["LateInteractionPooling"]
